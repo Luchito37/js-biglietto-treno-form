155210705538d98a0089d7abf0a96368d7353bae
Luchito37 <lucamatteotti01@gmail.com>
2022-05-19 17:58:56 +0200

    non so come procedere

---
+++ styles.js
@@ -0,0 +1,56 @@
+const inputName = document.querySelector("[name='userNameSurname']");
+let inputkmDistance = document.querySelector ("[name='userKmDistance']");
+const selectOption = document.getElementById("fasciaEta")
+const submitButton = document.getElementById("generatButton") ;
+
+
+const prezzoAlKm = "0.21";
+
+const scontoGiovani = "0.2";
+
+let prezzoBiglietto = inputkmDistance * prezzoAlKm
+
+const scontoAnziani = "0.4";
+
+//prezzoBiglietto - ( prezzoBiglietto * scontoAnziani )
+
+
+/* se la persona ha meno di 18 anni applica sconto */
+
+if(selectOption === "minorenne"){
+    prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * scontoGiovani )
+    console.log("giovane!", selectOption)
+}
+if(selectOption === "over 65"){
+    prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * scontoAnziani )
+}
+
+
+const spanPrezzoBiglietto = document.getElementById("span-prezzo-biglietto")
+
+spanPrezzoBiglietto.innerHTML = `<strong class="text-danger">
+                                    ${prezzoBiglietto}
+                                </strongs>`;
+
+
+const spanNomeCognomeUtente = document.getElementById("nome-cognome-utente");
+
+spanNomeCognomeUtente.innerHTML = `<strong class="text-danger">
+                                    <small class="text-dark">${inputName}</small>
+                                </strongs>`;
+
+/*
+la funzione addEventListener si aspetta di ricevere tra parentesi, 2 argomenti:
+il primo argomento (valore) è il nome dell'evento che si vuole ascoltare (click)
+- il secondo argomento (valore) è la funzione che si vuole eseguire quando l'evento viene generatol
+*/
+submitButton. addEventListener ("click", function () {
+    // Questo codice verrà eseguito SOLO quando l'utente clicca sul pulsante submitButton
+    console. log(inputName. value, inputkmDistance. value, selectOption. value);
+
+    inputName. value = " " ;
+
+    inputkmDistance. value = " " ;
+
+    selectOption. value = " " ;
+})
