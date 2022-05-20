28f81df5955539543e2eea165f24b373cb107410
Luchito37 <lucamatteotti01@gmail.com>
2022-05-20 10:31:10 +0200

    commit matutine

--- styles.js
+++ styles.js
@@ -1,42 +1,16 @@
 const inputName = document.querySelector("[name='userNameSurname']");
 let inputkmDistance = document.querySelector ("[name='userKmDistance']");
-const selectOption = document.getElementById("fasciaEta")
-const submitButton = document.getElementById("generatButton") ;
+const selectOption = document.getElementById("fascia-eta");
+const submitButton = document.getElementById("generat-button") ;
 
 
 const prezzoAlKm = "0.21";
 
 const scontoGiovani = "0.2";
-
-let prezzoBiglietto = inputkmDistance * prezzoAlKm
-
+    
 const scontoAnziani = "0.4";
 
 //prezzoBiglietto - ( prezzoBiglietto * scontoAnziani )
-
-
-
-if(selectOption === "minorenne"){
-    prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * scontoGiovani )
-}
-if(selectOption === "over-65"){
-    prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * scontoAnziani )
-}
-
-
-const spanPrezzoBiglietto = document.getElementById("span-prezzo-biglietto")
-
-spanPrezzoBiglietto.innerHTML = `<strong class="text-danger">
-                                    ${prezzoBiglietto}
-                                </strongs>`;
-
-
-const spanNomeCognomeUtente = document.getElementById("nome-cognome-utente");
-
-spanNomeCognomeUtente.innerHTML = `<strong class="text-danger">
-                                    <small class="text-dark">${inputName}</small>
-                                </strongs>`;
-
 
 
 submitButton. addEventListener ("click", function () {
@@ -48,4 +22,28 @@ submitButton. addEventListener ("click", function () {
     inputkmDistance. value = " " ;
 
     selectOption. value = " " ;
+
+    let prezzoBiglietto = inputkmDistance * prezzoAlKm
+
+
+    if(selectOption === "minorenne"){
+        prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * scontoGiovani )
+    }
+    if(selectOption === "over-65"){
+        prezzoBiglietto = prezzoBiglietto - ( prezzoBiglietto * scontoAnziani )
+    }
+
+    const spanPrezzoBiglietto = document.getElementById("span-prezzo-biglietto")
+
+    spanPrezzoBiglietto.innerHTML = `<strong class="text-danger">
+                                        ${prezzoBiglietto}
+                                    </strongs>`;
+
+
+    const spanNomeCognomeUtente = document.getElementById("nome-cognome-utente");
+
+    spanNomeCognomeUtente.innerHTML = `<strong class="text-danger">
+                                            <small class="text-dark">${inputName}</small>
+                                        </strongs>`;
+
 })
